930024eef89e6959dbdf2c3b19ee081dc0122830
SreejithaN <55690711+SreejithaN@users.noreply.github.com>
2019-10-16 10:05:01 +0530

    instructor-hibernate-create-delete

---
+++ instructor-hibernate/src/main/java/com/mycompany/instructor_hibernate/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.instructor_hibernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
